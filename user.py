a0e1a6d37f187eb1a1cd6899bfff4f62215cbd01
Aidin2005 <150506914+Aidin2005@users.noreply.github.com>
2024-01-30 11:45:31 +0600

    Create user.py

---
+++ user.py
@@ -0,0 +1,8 @@
+def main():
+    user_info={}
+    user_info["name"] = input("What is user's name? ")
+    user_info["age"] = input("What is user's age? ")
+    user_info["country_of_birth"] = input("what is the user's country of birth? ")
+    user_info["known_for"] = input("what is the user's known for? ")
+    print(user_info)
+main()
